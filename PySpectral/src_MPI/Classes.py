e0a4f6e8028439aef0cbe6cb0372fd9c3c4c5776
Eric <eric.parish9@gmail.com>
2016-10-06 09:41:03 -0400

    forgot to add classes to last commit

--- PySpectral/src_MPI/Classes.py
+++ PySpectral/src_MPI/Classes.py
@@ -544,3 +544,87 @@ class utilitiesClass():
     w0_w[:,:,:] = -(1j*grid.k1[:,None,None]*tauhat[4] + 1j*grid.k2[None,:,None]*tauhat[5] + 1j*grid.k3[None,None,:]*tauhat[2]) + 1j*grid.k3[None,None,:]*tau_projection
     return w0_u,w0_v,w0_w
 
+
+  def computeF(self,main,grid,myFFT):
+    #function to compute F (it is just the right hand side computation)
+    comm = MPI.COMM_WORLD
+    num_processes = comm.Get_size()
+    mpi_rank = comm.Get_rank()
+
+
+    myFFT.myifft3D(main.uhat,main.u)
+    myFFT.myifft3D(main.vhat,main.v)
+    myFFT.myifft3D(main.what,main.w)
+
+    myFFT.myfft3D(main.u*main.u,main.NL[0])
+    myFFT.myfft3D(main.v*main.v,main.NL[1])
+    myFFT.myfft3D(main.w*main.w,main.NL[2])
+    myFFT.myfft3D(main.u*main.v,main.NL[3])
+    myFFT.myfft3D(main.u*main.w,main.NL[4])
+    myFFT.myfft3D(main.v*main.w,main.NL[5])
+
+    phat  = grid.ksqr_i*( -grid.k1[:,None,None]*grid.k1[:,None,None]*main.NL[0] - grid.k2[None,:,None]*grid.k2[None,:,None]*main.NL[1] - \
+             grid.k3[None,None,:]*grid.k3[None,None,:]*main.NL[2] - 2.*grid.k1[:,None,None]*grid.k2[None,:,None]*main.NL[3] - \
+             2.*grid.k1[:,None,None]*grid.k3[None,None,:]*main.NL[4] - 2.*grid.k2[None,:,None]*grid.k3[None,None,:]*main.NL[5] )
+
+    if (main.rotate == 1):
+      phat[:,:,:] = phat[:,:,:] - 2.*grid.ksqr_i*1j*( grid.k1[:,None,None]*(main.vhat*main.Om3 - main.what*main.Om2) +
+                    grid.k2[None,:,None]*(main.what*main.Om1 - main.uhat*main.Om3) + \
+                    grid.k3[None,None,:]*(main.uhat*main.Om2 - main.vhat*main.Om1))
+
+    main.F[0] = myFFT.dealias( -1j*grid.k1[:,None,None]*main.NL[0] - 1j*grid.k2[None,:,None]*main.NL[3] - 1j*grid.k3[None,None,:]*main.NL[4] - \
+                                         1j*grid.k1[:,None,None]*phat - main.nu*grid.ksqr*main.uhat ,grid)
+
+    main.F[1] = myFFT.dealias(-1j*grid.k1[:,None,None]*main.NL[3] - 1j*grid.k2[None,:,None]*main.NL[1] - 1j*grid.k3[None,None,:]*main.NL[5] - \
+                                         1j*grid.k2[None,:,None]*phat - main.nu*grid.ksqr*main.vhat ,grid)
+
+    main.F[2] = myFFT.dealias( -1j*grid.k1[:,None,None]*main.NL[4] - 1j*grid.k2[None,:,None]*main.NL[5] - 1j*grid.k3[None,None,:]*main.NL[2] - \
+                                         1j*grid.k3[None,None,:]*phat - main.nu*grid.ksqr*main.what ,grid)
+
+    if (main.rotate == 1):
+      main.F[0] = main.F[0] + 2.*(main.vhat*main.Om3 - main.what*main.Om2)
+      main.F[1] = main.F[1] + 2.*(main.what*main.Om1 - main.uhat*main.Om3)
+      main.F[2] = main.F[2] + 2.*(main.uhat*main.Om2 - main.vhat*main.Om1)
+    #=========================================================================
+
+    uhat_f = grid.filter(main.uhat)
+    vhat_f = grid.filter(main.vhat)
+    what_f = grid.filter(main.what)
+    u_f = np.zeros(np.shape(main.u))
+    v_f = np.zeros(np.shape(main.v))
+    w_f = np.zeros(np.shape(main.w))
+    myFFT.myifft3D(uhat_f,u_f)
+    myFFT.myifft3D(vhat_f,v_f)
+    myFFT.myifft3D(what_f,w_f)
+
+    myFFT.myfft3D(u_f*u_f,main.NL[0])
+    myFFT.myfft3D(v_f*v_f,main.NL[1])
+    myFFT.myfft3D(w_f*w_f,main.NL[2])
+    myFFT.myfft3D(u_f*v_f,main.NL[3])
+    myFFT.myfft3D(u_f*w_f,main.NL[4])
+    myFFT.myfft3D(v_f*w_f,main.NL[5])
+
+    phat_f  = grid.ksqr_i*( -grid.k1[:,None,None]*grid.k1[:,None,None]*main.NL[0] - grid.k2[None,:,None]*grid.k2[None,:,None]*main.NL[1] - \
+             grid.k3[None,None,:]*grid.k3[None,None,:]*main.NL[2] - 2.*grid.k1[:,None,None]*grid.k2[None,:,None]*main.NL[3] - \
+             2.*grid.k1[:,None,None]*grid.k3[None,None,:]*main.NL[4] - 2.*grid.k2[None,:,None]*grid.k3[None,None,:]*main.NL[5] )
+
+    if (main.rotate == 1):
+      phat_f[:,:,:] = phat_f[:,:,:] - 2.*grid.ksqr_i*1j*( grid.k1[:,None,None]*(vhat_f*main.Om3 - what_f*main.Om2) +
+                    grid.k2[None,:,None]*(what_f*main.Om1 - uhat_f*main.Om3) + \
+                    grid.k3[None,None,:]*(uhat_f*main.Om2 - vhat_f*main.Om1))
+
+    main.F[0] = main.F[0] - myFFT.dealias( -1j*grid.k1[:,None,None]*main.NL[0] - 1j*grid.k2[None,:,None]*main.NL[3] - 1j*grid.k3[None,None,:]*main.NL[4] - \
+                                         1j*grid.k1[:,None,None]*phat_f - main.nu*grid.ksqr*uhat_f ,grid)
+
+    main.F[1] = main.F[1] - myFFT.dealias(-1j*grid.k1[:,None,None]*main.NL[3] - 1j*grid.k2[None,:,None]*main.NL[1] - 1j*grid.k3[None,None,:]*main.NL[5] - \
+                                         1j*grid.k2[None,:,None]*phat_f - main.nu*grid.ksqr*vhat_f ,grid)
+
+    main.F[2] = main.F[2] - myFFT.dealias( -1j*grid.k1[:,None,None]*main.NL[4] - 1j*grid.k2[None,:,None]*main.NL[5] - 1j*grid.k3[None,None,:]*main.NL[2] - \
+                                         1j*grid.k3[None,None,:]*phat_f - main.nu*grid.ksqr*what_f ,grid)
+
+    if (main.rotate == 1):
+      main.F[0] = main.F[0] - 2.*(vhat_f*main.Om3 - what_f*main.Om2)
+      main.F[1] = main.F[1] - 2.*(what_f*main.Om1 - uhat_f*main.Om3)
+      main.F[2] = main.F[2] - 2.*(uhat_f*main.Om2 - vhat_f*main.Om1)
+
+
